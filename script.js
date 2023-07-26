d9944541536727efc2a9fc7163d8ee4f11c5a9b4
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-07-26 21:45:16 +0530

    Update script.js
    Added all the buttons

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 <script>
-// It turns on/off a brian class
+// When eyes button is clicked, toggle laser class on brain
 $(".flash").click(function() {
   $(".brain").toggleClass('laser');
 });
 
-$(".color").click(function () {
-  // We Generate a Random number & use it in rgb() value;
+// When color button is clicked...
+$(".color").click(function() {
+  // assign each named color a random number 0-255
   var red = Math.floor(Math.random() * 255);
-  var blue = Math.floor(Math.random() * 255);
   var green = Math.floor(Math.random() * 255);
-//
-  alert(red + "," + green + "," + blue);
-  background.style = rgb(red, blue, green, 1);
+  var blue = Math.floor(Math.random() * 255);
+  
+  //Generate on RGBA value from red, green, and blue
+  // This value is used to generate colors
+  var randomRGBA = 'rgba('+red+','+green+','+blue+',1)';
+  
+  //We use jquery to change the background color
+    $("body").css("background", randomRGBA);
+    
+  //Display the three values in an alert window
+ // alert(randomRGBA);
+});
+
+$(".moves").click(function() {
+  $("img").toggle();
 });
 </script>
